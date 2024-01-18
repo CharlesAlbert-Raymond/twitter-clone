645523e8f781d54dde1e2b0b6f0daa78f711ed0f
Charles-Albert Raymond <charlesalbert.raymond97@gmail.com>
2024-01-18 14:27:05 -0500

    trpc setup

---
+++ src/server/api/routers/posts.ts
@@ -0,0 +1,8 @@
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+
+export const postsRouter = createTRPCRouter({
+    getAll: publicProcedure.query(({ ctx }) => {
+        return ctx.db.post.findMany();
+    }),
+})
