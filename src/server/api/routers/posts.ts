8b7916633e8aa0c8fbd019866fcd393b7f802b16
Charles-Albert Raymond <charlesalbert.raymond97@gmail.com>
2024-01-19 15:54:08 -0500

    remove emoji condition

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -48,7 +48,7 @@ export const postsRouter = createTRPCRouter({
     });
   }),
   create: privateProcedure
-    .input(z.object({ content: z.string().emoji().min(1).max(280) }))
+    .input(z.object({ content: z.string().min(1).max(280) }))
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId;
 
